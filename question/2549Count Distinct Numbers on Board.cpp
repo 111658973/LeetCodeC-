18299e2a0ea9d982fb5bd48ca6545a353e75f65a
cbf <806028258@qq.com>
2024-06-28 16:04:45 +0800

    init

---
+++ question/2549Count Distinct Numbers on Board.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Cc on 2024/3/23.
+//
+
+class Solution {
+public:
+    static int distinctIntegers(int n) {
+        int count =1;
+        if(n==2){ count++; }
+        else()
+    }
+
+    void 
+
+
+};
+
+int main(){
+    Solution::distinctIntegers(5);
+}
